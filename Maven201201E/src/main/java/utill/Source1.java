28c18bb39a03916864811d2220ecdc8af3da0982
Root <Oni-Tani@shamim-go-pro.lan>
2021-03-16 19:16:44 -0400

    hi

---
+++ Maven201201E/src/main/java/utill/Source1.java
@@ -0,0 +1,11 @@
+package utill;
+
+public class Source1 {
+
+	
+	
+	public void m1(){
+System.out.println("hi");
+
+	}
+}
---
+++ Maven201201E/src/test/java/com/SmokeTest.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.junit.Test;
+
+import utill.Source1;
+
+public class SmokeTest {
+
+	
+	
+	@Test
+	public void test1() {
+		Source1 s =new Source1();
+		s.m1();
+	}
+}
